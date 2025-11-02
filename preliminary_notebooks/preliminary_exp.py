4d1df70cb535b397ae81da3cc3f0975d2713fcd5
Aelrach <raphaelsup@outlook.com>
2025-11-02 11:00:13 +0100

    Created code to efficiently run the Circle latent experiment, done for SVAE only for now

--- preliminary_notebooks/preliminary_exp.py
+++ preliminary_notebooks/preliminary_exp.py
@@ -30,7 +30,8 @@ if __name__ == "__main__":
 
     print("Started training..")
     model_svae, losses = training_loop(dataloader, model_svae, optimizer, 
-                                  epochs=50, beta_kl=0.1)
+                                  epochs=50, beta_kl=0.1,
+                                  show_loss_every=1)
 
     # visualisation espace latent
     with torch.no_grad():
@@ -41,10 +42,10 @@ if __name__ == "__main__":
         print("Sampling from latent space..")
         latent_samples = []
         for i in range(data_tensor.size(0)):
-            z = sample_vmf(mu_all[i,:], kappa_all[i,:], 1)
-            latent_samples(z.numpy())
+            z = sample_vmf(mu_all[i:i+1,:], kappa_all[i:i+1,:], 1)
+            latent_samples.append(z.numpy())
         latent_samples = np.concat(latent_samples, axis=0)
 
     print("Plotting latent spaces..")
     plot_latent_space(data_2d, latent_samples, labels, losses, 
-                      save="SVAE_compare_true_learned.pdf")
+                      save_path="SVAE_compare_true_learned.pdf")
